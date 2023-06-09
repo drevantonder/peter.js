46409a0243d32ac4fa1f962b60fa18be826b265a
Andre van Tonder <andre@drevan.me>
2023-06-09 20:43:07 +1000

    Configure eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,10 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
-    },
-  },
+  extends: [
+    'turbo',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
 };
