7976039cfe027ee4c1f7d2d06fd26b10782a5797
Andre van Tonder <andre@drevan.me>
2023-06-19 21:06:40 +1000

    Style up components to make them a little more presentable

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -26,13 +26,18 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        <header>
-          <nav>
-            <ul>
+        <header className="bg-white">
+          <nav className="container mx-auto px-4 py-4 flex items-center">
+            <ul className="flex space-x-8">
               {headerNavMenu?.items?.map((item) => (
                 <li key={item._key}>
                   {item._type == 'navigationItem' && item.link.isExternal && (
-                    <a href={item.link.externalUrl}>{item.title}</a>
+                    <a
+                      href={item.link.externalUrl}
+                      className="text-neutral-900 font-semibold text-xl"
+                    >
+                      {item.title}
+                    </a>
                   )}
                   {item._type == 'navigationItem' && !item.link.isExternal && (
                     <Link
@@ -41,6 +46,7 @@ export default async function RootLayout({
                           ? item.link.internalLink.slug.current
                           : ''
                       }`}
+                      className="text-neutral-900 font-semibold text-xl"
                     >
                       {item.title}
                     </Link>
@@ -51,29 +57,41 @@ export default async function RootLayout({
           </nav>
         </header>
         {children}
-        <footer>
-          <nav>
-            <ul>
-              {footerNavMenu?.items?.map((item) => (
-                <li key={item._key}>
-                  {item._type == 'navigationItem' && item.link.isExternal && (
-                    <a href={item.link.externalUrl}>{item.title}</a>
-                  )}
-                  {item._type == 'navigationItem' && !item.link.isExternal && (
-                    <Link
-                      href={`/${
-                        item.link.internalLink.slug
-                          ? item.link.internalLink.slug.current
-                          : ''
-                      }`}
-                    >
-                      {item.title}
-                    </Link>
-                  )}
-                </li>
-              ))}
-            </ul>
-          </nav>
+        <footer className="bg-white">
+          <div className="container mx-auto px-4 py-8 flex justify-center items-center flex-col">
+            <nav>
+              <ul className="flex items-center space-x-12">
+                {footerNavMenu?.items?.map((item) => (
+                  <li key={item._key}>
+                    {item._type == 'navigationItem' && item.link.isExternal && (
+                      <a
+                        href={item.link.externalUrl}
+                        className="text-neutral-600 text-sm"
+                      >
+                        {item.title}
+                      </a>
+                    )}
+                    {item._type == 'navigationItem' &&
+                      !item.link.isExternal && (
+                        <Link
+                          href={`/${
+                            item.link.internalLink.slug
+                              ? item.link.internalLink.slug.current
+                              : ''
+                          }`}
+                          className="text-neutral-600 text-sm"
+                        >
+                          {item.title}
+                        </Link>
+                      )}
+                  </li>
+                ))}
+              </ul>
+            </nav>
+            <p className="mt-6 text-center text-xs leading-5 text-gray-500">
+              &copy; 2023 Your Company, Inc. All rights reserved.
+            </p>
+          </div>
         </footer>
       </body>
     </html>
