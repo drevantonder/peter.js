9f475467353331c228c7ff71719631fe9327aeaf
Andre van Tonder <andre@drevan.me>
2023-06-20 20:59:33 +1000

    Update types

--- apps/web/app/components/hero.tsx
+++ apps/web/app/components/hero.tsx
@@ -4,19 +4,21 @@ import { SanityImage } from './SanityImage'
 export const heroSelection = {
   title: q.string(),
   subtitle: q.string(),
-  ctas: q.array(
-    q.object({
-      title: q.string(),
-      link: q.string().url(),
-    })
-  ),
+  ctas: q
+    .array(
+      q.object({
+        title: q.string(),
+        link: q.string().url(),
+      })
+    )
+    .nullable(),
   image: sanityImage('image', {
     withHotspot: true,
     isList: false,
     additionalFields: {
       alt: q.string(),
     },
-  }),
+  }).nullable(),
 } satisfies Selection
 
 type Props = {
@@ -26,14 +28,16 @@ type Props = {
 export function Hero({ content }: Props) {
   return (
     <div className="relative aspect-video flex items-center justify-center">
-      <div className="absolute inset-0 -z-10">
-        <SanityImage
-          src={content.image.asset._ref}
-          alt={content.image.alt}
-          placeholder="blur"
-          fill={true}
-        />
-      </div>
+      {content.image && (
+        <div className="absolute inset-0 -z-10">
+          <SanityImage
+            src={content.image.asset._ref}
+            alt={content.image.alt}
+            placeholder="blur"
+            fill={true}
+          />
+        </div>
+      )}
       <div className="bg-neutral-200/10 backdrop-blur rounded-lg overflow-clip p-8">
         <h1 className="text-6xl font-extrabold text-neutral-900">
           {content.title}
@@ -41,13 +45,15 @@ export function Hero({ content }: Props) {
         <h2 className="text-2xl font-semibold text-neutral-800">
           {content.subtitle}
         </h2>
-        <ul>
-          {content.ctas.map((cta, index) => (
-            <li key={index}>
-              <a href={cta.link}>{cta.title}</a>
-            </li>
-          ))}
-        </ul>
+        {content.ctas && (
+          <ul>
+            {content.ctas.map((cta, index) => (
+              <li key={index}>
+                <a href={cta.link}>{cta.title}</a>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   )
